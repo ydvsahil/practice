053cd9c6ebbdb8215e33a2ad71033fdd534073b6
Sahil Yadav <sahilyadavnikum@gmail.com>
2022-01-15 02:11:14 +0530

    first java file

---
+++ temp.java
@@ -0,0 +1,17 @@
+class firstClass {
+	private Integer a;
+	private List<Integer> arr;
+
+	public void setA(Integer a) {
+		this.a = a;
+	}
+	public Integer getA() {
+		return a;
+	}
+	public void setArr(List<Integer> arr) {
+		this.arr = arr;
+	}
+	public List<Integer> getArr() {
+		return arr;
+	}
+}
